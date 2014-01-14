2bc6c483031040b918cbc4d55c14a221abef05c3
Cameron White <cawhite@pdx.edu>
2014-01-13 22:28:09 -0800

    Now using BotParse

--- modules/acmbot.py
+++ modules/acmbot.py
@@ -8,6 +8,7 @@ import urllib
 from StringIO import StringIO
 from kitnirc.modular import Module
 from bs4 import BeautifulSoup
+from botparse import BotParse
  
 _log = logging.getLogger(__name__)
 
@@ -16,21 +17,8 @@ ACM_BASE_URL = 'http://acm.pdx.edu'
 
 parser_output = StringIO()
 
-class ArgumentParser(argparse.ArgumentParser):
-    def _print_message(self, message, file=None):
-        super(ArgumentParser, self)._print_message(message, parser_output)
-    def exit(self, status=0, message=None):
-        if message:
-            self._print_message(message, None)
-
-parser = ArgumentParser(add_help=False)
-subparsers = parser.add_subparsers(dest='subparser')
-subparser_events = subparsers.add_parser('!events', add_help=False)
-subparser_events.add_argument(
-    '--help', default=False, action='store_const', const=True,
-    help="print help message"
-)
-subparser_help = subparsers.add_parser('!help', add_help=False)
+parser = BotParse()
+command_events = parser.add_command('!events')
 
 def reset_parser_output():
     parser_output.seek(0, 0)
@@ -63,9 +51,9 @@ class AcmBotModule(Module):
         # http://docs.python.org/2/library/logging.html
         _log.info("Responding to %r in %r", self.actor, self.recipient)
         
-        if args.subparser == "!events":
+        if args.command == "!events":
             if args.help:
-                messages = subparser_events.format_help().split('\n')
+                messages = command_events.format_help().split('\n')
             else:
                 html = urllib.urlopen(ACM_BASE_URL + '/events.php').read()
                 soup = BeautifulSoup(html)
@@ -73,7 +61,7 @@ class AcmBotModule(Module):
                     soup.ui.text.replace('\n', '').strip(),
                     ACM_BASE_URL + soup.ui.a['href'],
                 ]
-        elif args.subparser == "!help":
+        elif args.command == "!help":
             messages = parser.format_help().split('\n')
 
         for message in messages:
