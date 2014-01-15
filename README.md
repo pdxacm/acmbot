dc081efa718d0eeb98e9757baecb1d44145c521e
Cameron White <cawhite@pdx.edu>
2014-01-15 00:56:41 -0800

    Install script + Install docs

--- README.md
+++ README.md
@@ -2,3 +2,20 @@ acmbot
 ======
 
 Bot for the PDX acm
+
+## Install
+
+### Installing in a virtualenv
+
+```sh 
+# 1. Clone acmbot
+git clone https://github.com/cameronbwhite/acmbot.git
+# 2. Enter Repo
+cd acmbot
+# 3. Create a virtual enviroment
+virtualenv venv
+# 4. Enter vittual enviroment
+source venv/bin/activate
+# 5. Install dependencies
+python setup.py install
+```
---
+++ setup.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Copyright (C) 2013, Cameron White
+import setuptools
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="acmbot",
+        version='0.1.0',
+        description="A bot for @pdxacm",
+        author="Cameron Brandon White",
+        author_email="cameronbwhite90@gmail.com",
+        provides=[
+            "acmbot",
+        ],
+        packages=[
+            "modules",
+        ],
+        py_modules = [
+            "bot",
+        ],
+        package_data = {
+            'PyOLP': ["LICENSE", "README.md", "config.cfg"],
+        },
+        install_requires = [
+            'kitnirc',
+            'BotParse',
+            'beautifulsoup4',
+        ],
+        include_package_data=True,
+    )
