0f1c23a15bd43fec53d3359cfce5bca9c00c89a4
Cameron Brandon White <cameronbwhite90@gmail.com>
2014-02-17 03:58:55 -0800

    Redisgned `modules/acmbot.py` decoupled code
    A lot of functionality has been moved out of the AcmBotModule class.
    
    * The loading of `events.yaml` is now done with the `load_yaml`
    function.
    * There are now filter functions which are responsible for sorting
    and selecting which events are wanted. There filters are built opon
    the select_events and sort_events functions
        * `select_events`
        * `sort_events`
        * `filter_take_date_range`
        * `filter_take_date`
    * There are now command functions which implement the verious things
    the acmbot can do.
        * `command_date`
        * `command_today`
        * `command_tomorrow`
        * `command_next`
        * `command_events`
        * `command_day`
    * utility functions have been added
        * `weekday_difference`
    
    The AcmBotModule is now responsible for responding to irc events,
    loading configuration, calling global function above, constructing
    messages, and responding to the irc event.

--- modules/acmbot.py
+++ modules/acmbot.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-# Copyright (C) 2013, Cameron White
+# Copyright (C) 2014, Cameron White
 
 import logging
 import argparse
@@ -10,7 +10,8 @@ from botparse import BotParse
 import yaml
 import datetime
 import urllib
-from itertools import takewhile
+from contextlib import closing
+from itertools import takewhile, dropwhile
  
 _log = logging.getLogger(__name__)
 
@@ -44,7 +45,7 @@ class AcmBotModule(Module):
         self.recipient = recipient
         self.message = message
 
-        config = self.controller.config
+        self.read_config()
 
         # Only pay attention if addressed directly in channels
         try:
@@ -57,119 +58,37 @@ class AcmBotModule(Module):
         # http://docs.python.org/2/library/logging.html
         _log.info("Responding to %r in %r", self.actor, self.recipient)
         
-        messages = []
-
         if self.args.command == "!events":
             if self.args.help:
                 messages = command_events.format_help().split('\n')
             else:
-                def sort(events):
-                    events = sorted(events, key=lambda x: x['date'], reverse=True)
-                    events = list(enumerate(takewhile(
-                        lambda x: x['date'] >= datetime.date.today(),
-                        events,
-                    )))
-                    return events
-                messages = self.get_event_messages(sort)
+                messages = self.do_command(command_events)
         
         elif self.args.command == '!today':
             if self.args.help:
-                messages = command_events.format_help().split('\n')
+                messages = command_today.format_help().split('\n')
             else:
-                def today_sort(events):
-                    events = sorted(events, key=lambda x: x['date'], reverse=True)
-                    events = list(enumerate(takewhile(
-                        lambda x: x['date'] >= datetime.date.today(),
-                        events,
-                    )))
-                    events = filter(
-                        lambda (i, event): event['date'] == datetime.date.today(),
-                        events
-                    )
-                    return events
-                messages = self.get_event_messages(today_sort)
+                messages = self.do_command(command_today)
 
         elif self.args.command == '!tomorrow':
             if self.args.help:
-                messages = command_events.format_help().split('\n')
+                messages = command_tomorrow.format_help().split('\n')
             else:
-                def tomorrow_sort(events):
-                    events = sorted(events, key=lambda x: x['date'], reverse=True)
-                    events = list(enumerate(takewhile(
-                        lambda x: x['date'] >= datetime.date.today() + datetime.timedelta(days=1),
-                        events,
-                    )))
-                    events = filter(
-                        lambda (i, event): event['date'] == datetime.date.today() + datetime.timedelta(days=1),
-                        events
-                    )
-                    return events
-                messages = self.get_event_messages(tomorrow_sort)
+                messages = self.do_command(command_tomorrow)
 
         elif self.args.command == '!next':
             if self.args.help:
-                messages = command_events.format_help().split('\n')
+                messages = command_next.format_help().split('\n')
             else:
-                def tomorrow_sort(events):
-                    events = sorted(events, key=lambda x: x['date'], reverse=True)
-                    events = list(enumerate(takewhile(
-                        lambda x: x['date'] >= datetime.date.today(),
-                        events,
-                    )))
-                    return [ events[-1] ]
-                messages = self.get_event_messages(tomorrow_sort)
+                messages = self.do_command(command_next)
 
         elif self.args.command == '!day':
             if self.args.help:
-                messages = command_events.format_help().split('\n')
+                messages = command_day.format_help().split('\n')
             elif self.args.day:
-                # The date to filter by will be constructed.
-
-                today = datetime.date.today()
-                
-                # Get the int encoding of the day of the week.
-                # Monday = 0 ... Sunday = 6
-                today_weekday = today.weekday()
-
-
-                wanted_weekday = [
-                    'monday', 'tuesday', 'wednesday', 'thursday',
-                    'friday', 'saturday', 'sunday',
-                ].index(self.args.day[0].lower())
-
-                if wanted_weekday >= today_weekday:
-                    # If today is Wednesday and Wednesday is wanted then
-                    # the day should not be changed.
-                    #     Wednesday - Wednesday = 2 - 2 = 0
-                    # If today is Tuesday and Friday is wanted then
-                    # the day should be increased by 3
-                    #     Friday - Tuesday = 4 - 1 = 3
-                    days = wanted_weekday - today_weekday
-                else:
-                    # If today is Friday and Monday is wanted then the
-                    # day should be increased by 3.
-                    #    7 - Friday + Monday = 7 - 4 + 0 = 3 
-                    # If today is Thursday and Tuesday is wanted then the
-                    # day should be increased by 5.
-                    #    7 - Thursday + Tuesday = 7 - 3 + 1 = 5
-                    days = 7 - today_weekday + wanted_weekday
-            
-                # Construct the date by adding the calculated number of
-                # days.
-                date = today + datetime.timedelta(days=days)
-
-                def day_sort(events):
-                    events = sorted(events, key=lambda x: x['date'], reverse=True)
-                    events = list(enumerate(takewhile(
-                        lambda x: x['date'] >= date,
-                        events,
-                    )))
-                    events = filter(
-                        lambda (i, event): event['date'] == date,
-                        events
-                    )
-                    return events
-                messages = self.get_event_messages(day_sort)
+                messages = self.do_command(
+                    lambda events: command_day(events, self.args.day),
+                )
 
         elif self.args.command == "!help":
             messages = parser.format_help().split('\n')
@@ -180,20 +99,16 @@ class AcmBotModule(Module):
 
         # Stop any other modules from handling this message.
         return True
-
-    def get_event_messages(self, func=None):
-        config = self.controller.config
         
-        if not config.has_section("acmbot"):
-            _log.info("No config section for acmbot")
-            return
+    def do_command(self, command):
 
-        if config.has_option("acmbot", "base_url"):
-            base_url = config.get("acmbot", "base_url")
-        else:
-            return
+        events = self.get_events()
+        
+        events = command(events)
 
-        if getattr(self.args, 'limit', None):
+        config = self.controller.config
+
+        if hasattr(self.args, 'limit'):
             events_limit = self.args.limit
         elif config.has_option("acmbot", "events_limit"):
             try:
@@ -203,44 +118,171 @@ class AcmBotModule(Module):
         else:
             events_limit = None
 
-        events_html = urllib.urlopen(
-            '{}/files/events.yaml'.format(base_url)
-        ).read()
-
-        events = yaml.load(events_html)
-        length_events = len(events)
-
-        if func:
-            events = func(events)
-        
         if events_limit and events_limit >= 0 and len(events) >= events_limit:
             events = events[len(events)-events_limit:]
-        
-        messages = []
+
         for i, event in reversed(events):
-            
-            if 'time' in event:
-                time = get_time(event['time'])
-            elif config.has_option("acmbot", "default_time"):
-                time = get_time(config.get("acmbot", "default_time"))
-            else:
-                time = None
+            yield self.construct_message(i, event)
+    
+    def construct_message(self, event_id, event):
 
-            message = '{} - {:%a, %b %d}'.format(
-                event['title'], event['date'],
-            )
+        if 'time' in event:
+            time = get_time(event['time'])
+        else:
+            time = None
 
-            if time:
-                message += ' @ {:%I:%M%p}'.format(time)
+        message = '{} - {:%a, %b %d}'.format(
+            event['title'], event['date'],
+        )
 
-            message += ' - {}/event.php?event={}'.format(
-                base_url, str(length_events - i - 1),
-            )
+        if time:
+            message += ' @ {:%I:%M%p}'.format(time)
 
-            messages.append(message)
-        return messages
+        message += ' - {}/event.php?event={}'.format(
+            self.base_url, str(self.number_of_events - event_id - 1),
+        )
+
+        return message
+
+
+    def get_events(self):
+        events = load_yaml(
+            '{}/{}'.format(self.base_url, self.events_yaml_url)
+        )
+        self.number_of_events = len(events)
+        return events
+
+    def read_config(self):
+        config = self.controller.config
+
+        if not config.has_section("acmbot"):
+            _log.error("config has no `acmbot` section")
+            return
+
+        if config.has_option("acmbot", "base_url"):
+            self.base_url = config.get("acmbot", "base_url")
+        else:
+            _log.error("config `acmbot` section has no `base_url` option")
+            return
+
+        if config.has_option("acmbot", "events_yaml_url"):
+            self.events_yaml_url = config.get("acmbot", "events_yaml_url")
+        else:
+            _log.error("config `acmbot` section has no `events_yaml_url")
+            return
+
+
+def load_yaml(url):
+    with closing(urllib.urlopen(url)) as yaml_raw:
+        return yaml.load(yaml_raw.read())
+
+def select_events(events, filter_function):
+    # Sort events from newest to oldest.
+    # Attach an id to each event [(id, event)].
+    events = enumerate(sort_events(events))
+    # Apply filtering function
+    events = filter_function(events)
+    return list(events)
+
+def sort_events(events):
+    """ Sort events from newest to oldest """
+    return sorted(events, key=lambda x: x['date'], reverse=True)
+
+def filter_take_date_range(events, start_date, end_date):
+    """ Events filter which takes events which dates in the range
+    from start_date to end_date inclusively. start_date >= end_date.
+    events must be a list of the form [(id, event)]. """
+   
+    if start_date:
+        events = dropwhile(
+            lambda x: x[1]['date'] > start_date,
+            events
+        )
+
+    if end_date:
+        events = takewhile(
+            lambda x: x[1]['date'] >= end_date,
+            events
+        )
+
+    return events
+
+def filter_take_date(events, date):
+    return filter_take_date_range(events, date, date)
+
+def command_date(events, date):
+    return select_events(
+            events, 
+            lambda x: filter_take_date(x, date)
+    )
+
+def command_today(events):
+    date = datetime.date.today()
+    return command_date(events, date)
+
+def command_tomorrow(events):
+    date = datetime.date.today() + datetime.timedelta(days=1)
+    return command_date(events, date)
+
+def command_day(events, weekday):
+    # The date to filter by will be constructed.
+
+    today = datetime.date.today()
+    
+    # Get the int encoding of the day of the week.
+    # Monday = 0 ... Sunday = 6
+    today_weekday = today.weekday()
+    
+    days = weekday_difference(today_weekday, weekday[0])
+
+    # Construct the date by adding the calculated number of
+    # days.
+    date = today + datetime.timedelta(days=days)
+    return command_date(events, date)
+
+def command_events(events):
+    date = datetime.date.today()
+    return select_events(
+            events, 
+            lambda events: filter_take_date_range(events, None, date)
+    )
+
+def command_next(events):
+    return command_events(events)[-1:]
+
+def weekday_difference(from_weekday, to_weekday):
+
+    def get_weekday_index(weekday):
+        if weekday not in range(7):
+            weekday = [
+                'monday', 'tuesday', 'wednesday', 'thursday',
+                'friday', 'saturday', 'sunday',
+            ].index(weekday.lower())
+        return weekday
+    
+    from_weekday = get_weekday_index(from_weekday)
+    to_weekday = get_weekday_index(to_weekday)
+
+    if to_weekday >= from_weekday:
+        # If from is Wednesday and Wednesday is to then
+        # the day should not be changed.
+        #     Wednesday - Wednesday = 2 - 2 = 0
+        # If from is Tuesday and Friday is to then
+        # the day should be increased by 3
+        #     Friday - Tuesday = 4 - 1 = 3
+        days = to_weekday - from_weekday
+    else:
+        # If from is Friday and Monday is to then the
+        # day should be increased by 3.
+        #    7 - Friday + Monday = 7 - 4 + 0 = 3 
+        # If from is Thursday and Tuesday is to then the
+        # day should be increased by 5.
+        #    7 - Thursday + Tuesday = 7 - 3 + 1 = 5
+        days = 7 - from_weekday + to_weekday
+
+    return days
 
 def get_time(time_str):
     return datetime.datetime.strptime(time_str, '%H:%M').time()
-    
+
 module = AcmBotModule
